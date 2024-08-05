3665f63d27e5d00563124720e69042f650341c86
Eric Vincent <eric@efvincent.com>
2024-08-05 08:58:38 -0400

    stub out test scene framework and initial test scene

---
+++ include/tests/testClearColor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "test.hpp"
+
+namespace Test {
+
+  class TestClearColor : Test
+  {
+  public:
+    TestClearColor();
+    ~TestClearColor();
+  
+    void OnUpdate(float deltaTime) override;
+    void OnRender() override;
+    void OnImGuiRender() override;
+  
+  private:
+    float m_color[4];
+  
+  };
+
+}
---
+++ src/tests/testClearColor.cpp
@@ -0,0 +1,23 @@
+#include "tests/testClearColor.hpp"
+#include "glAssert.hpp"
+#include <imgui.h> 
+
+namespace Test {
+
+    TestClearColor::TestClearColor()
+      : m_color { 0.2f, 0.3f, 0.8f, 1.0f } {}
+
+    TestClearColor::~TestClearColor() { }
+
+    void TestClearColor::OnUpdate(float deltaTime) { }
+
+    void TestClearColor::OnRender() {
+      GLCall(glClearColor(m_color[0], m_color[1], m_color[2], m_color[3]));
+      GLCall(glClear(GL_COLOR_BUFFER_BIT));
+    }
+
+    void TestClearColor::OnImGuiRender() { 
+      ImGui::ColorEdit4("Clear Color", m_color);
+    }
+
+}
