7400d7c98c2c782f957a078707b50b12c5497c3a
E F Vincent <eric.vincent@aetion.com>
2024-08-05 10:28:22 -0400

    add a name to the test superclass

--- include/tests/test.hpp
+++ include/tests/test.hpp
@@ -10,5 +10,6 @@ namespace Test {
     virtual void OnUpdate(float deltaTime) {} 
     virtual void OnRender() {}
     virtual void OnImGuiRender() {}
+    virtual const char* testName() const { return "N/A"; };
   };
 }
